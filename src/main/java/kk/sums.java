c32d8daf965adce87b0dad4fa1e9ce9419ddaccd
iskhakovrr <34068634+iskhakovrr@users.noreply.github.com>
2018-04-24 23:20:23 +0500

    13

--- src/main/java/kk/sums.java
+++ src/main/java/kk/sums.java
@@ -2,7 +2,7 @@ package kk;
 
 public class sums {
 	public static float sums_rl(float k1){
-		float sum =(k1*1.2)/220;
+		float sum =(k1*1.2f)/220;
 		return sum;
 	}
 }
--- src/main/java/kk/sums1.java
+++ src/main/java/kk/sums1.java
@@ -2,7 +2,7 @@ package kk;
 
 public class sums1 {
 	public static float sums_rl1(float k11){
-		float sum1=(k11*1.2)/380;
+		float sum1=(k11*1.2f)/380;
 		return sum1;
 	}
 }
