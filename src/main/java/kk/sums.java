5697a7a86fe166689cf77c819a4407636316c06e
iskhakovrr <34068634+iskhakovrr@users.noreply.github.com>
2018-04-24 21:18:58 +0500

    2

---
+++ src/main/java/kk/sums.java
@@ -0,0 +1,14 @@
+package kk;
+
+public class sums {
+	public static float sums_rl(float k1){
+		float sum;
+		sum = (float) ((k1*1.2)/220);
+		return sum;
+	}
+	public static float sums_rl1(float k1){
+		float sum;
+		sum = (float) ((k1*1.2)/380);
+		return sum;
+	}
+}
