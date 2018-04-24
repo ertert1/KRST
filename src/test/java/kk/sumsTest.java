94617c22d70a39dbb9d3c3a77b088e166b19a836
iskhakovrr <34068634+iskhakovrr@users.noreply.github.com>
2018-04-25 00:57:55 +0500

    15

--- src/test/java/kk/sumsTest.java
+++ src/test/java/kk/sumsTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class test1 {
+class sumsTest {
 	
 	 sums testingClass;
 
